a23e3951189a181c7c18513c4e53a7ad6ad83649
rioze <rioze@192.168.170.128>
2016-10-20 22:29:53 +0800

    ext 首页改为index.jsp

---
+++ src/main/webapp/scripts/build/temp/production/crm/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'C:\Users\rioze\git\crm\src\main\webapp\scripts/build/.sass-cache'
---
+++ src/main/webapp/scripts/build/temp/production/crm/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Users\rioze\git\crm\src\main\webapp\scripts\app.js

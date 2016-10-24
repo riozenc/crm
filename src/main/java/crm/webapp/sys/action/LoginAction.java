e892b11b1338c0cb63362b8b7a961cc1b26371e4
rioze <rioze@192.168.170.128>
2016-10-24 23:07:55 +0800

    认证功能成功

--- src/main/java/crm/webapp/sys/action/LoginAction.java
+++ src/main/java/crm/webapp/sys/action/LoginAction.java
@@ -5,6 +5,8 @@
  */
 package crm.webapp.sys.action;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,7 +23,16 @@ import crm.common.webapp.base.action.BaseAction;
 public class LoginAction extends BaseAction {
 
 	@RequestMapping(value = "/login")
-	public String login(String username, String password, String userpass) {
+	public String login(String username, String password, String userpass,HttpServletRequest httpServletRequest) {
+
+		String errorClassName = (String) httpServletRequest.getAttribute("shiroLoginFailure");
+		
+//		DisabledAccountException （禁用的帐号）
+//		LockedAccountException （锁定的帐号）
+//		UnknownAccountException（错误的帐号）
+//		ExcessiveAttemptsException（登录失败次数过多）
+//		IncorrectCredentialsException （错误的凭证）
+//		ExpiredCredentialsException （过期的凭证）
 
 		System.out.println(username);
 		System.out.println(password);
